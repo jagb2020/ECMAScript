5c55575ff312f1fb0f19ff2735a12441497339c7
Angel Gonzalez <agb92@live.com>
2020-12-20 19:20:02 -0600

    ¿Qué se implementó en ES7?

---
+++ clase5.js
@@ -0,0 +1,18 @@
+/* es7 */
+/* include */
+let numbers = [1,2,3,7,8]
+
+if (numbers.includes(9)) { // con 7 se encuntra el valor con 9 no se encuentra
+    console.log('si se encuentra el valor 7')
+}else {
+    console.log('Nos se encuentra')
+}
+
+/* numero exponencial */
+let base = 4
+let exponent = 3
+let result = base ** exponent
+
+console.log(result)
+//4 a la potencia 3 = 64
+//4 a la pitencia 4 = 256
